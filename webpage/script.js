b67ff21bcd71ec75e3a3736cc31835914a0085ef
Saudahmed02 <saudahmeds778@gmail.com>
2022-01-01 21:22:52 -0600

    update

--- webpage/script.js
+++ webpage/script.js
@@ -1,9 +1,72 @@
+///Creating an array
+var registeredAccounts =
+[
+    {
+        username: "john",
+        password: "12pass"
+    },
 
-//Event Handler
+    {
+        username: "smith",
+        password: "coder"
+    },
+
+    {
+        username: "sam",
+        password: "coding"
+    }
+];
+
+
+
+//Login Information
 function getInfo(){
 
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
+    var user = document.getElementById('username').value;
+    var pass = document.getElementById('password').value;
 
-    console.log("Your username" + username + " and password" + password);
+
+    //Checking the function if the user and password are correct
+    for (var i = 0; i < registeredAccounts.length; i++)
+    {
+        if((user == registeredAccounts[i].username) && (pass == registeredAccounts[i].password))
+        {
+            console.log(user + " successfully logged in!");
+            return;
+        }
+    }
+
+    console.log("Incorrect username or password");
+
+
+};
+
+
+
+//Function that registers student accounts
+function registerUser()
+{
+    var registerUser = document.getElementById('newUsername').value;
+    var registerPassword = document.getElementById('newPassword').value;
+
+    var newUser = {
+        username: registerUser,
+        password: registerPassword
+    }
+
+    for(i = 0; i < registeredAccounts.length; i++)
+    {
+        if(registerUser == registeredAccounts[i].username)
+        {
+            alert("That Username already exits, please choose another");
+            return;
+
+        }else if (registerPassword.length < 8){
+            alert("that password is too short, include 8 or more characters");
+            return;
+        }
+    }
+
+    registeredAccounts.push(newUser);
+    console.log(registeredAccounts);
 }
